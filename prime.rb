643b25e236fcfb1303c127970a7430e09c4b40f3
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-06-07 10:56:24 -0400

    done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+require 'pry'
+def prime?(num)
+    if num < 0 || num == 0 || num == 1
+        return false
+      else
+    (2..(num-1)).each do |n|
+        return false if num % n == 0
+    end
+    true
+end
+end
